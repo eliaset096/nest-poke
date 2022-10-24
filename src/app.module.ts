d8300be03ef7adf9c6eb66a6a53e442645b5c327
Elías Estupiñan <eliaset096@gmail.com>
2022-10-24 02:03:30 -0500

    feat: initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonsModule } from './pokemons/pokemons.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PokemonsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
